f609410fb59829a7165a30d3fb2940ddb902a26d
nthomson <nthomsondev@gmail.com>
2011-05-23 23:30:00 -0400

    Add retrieving annotations from URLs

--- scripts/annotate.js
+++ scripts/annotate.js
@@ -54,57 +54,8 @@ $(document).ready(
 					//Get X/Y with chat-arrow and page offsets
 					var x = e.pageX - this.offsetLeft - 25;
 					var y = e.pageY - this.offsetTop + 10;
-					
-					//Create textbox
-					var textBox = $(document.createElement('textarea'));
-					$(textBox).keyup(
-						//Grow textbox with content
-						function() {
-						   /* Make sure element does not have scroll bar to prevent jumpy-ness */
-						   if (this.style.overflowY != 'hidden') { this.style.overflowY = 'hidden' }
-						   /* Now adjust the height */
-						   var scrollH = this.scrollHeight;
-						   if( scrollH > this.style.height.replace(/[^0-9]/g,'') ){
-							  this.style.height = scrollH+'px';
-						   }
-						}
-					);
-					
-					//Create annotation
-					var annotation = $(document.createElement('div')).attr('class', 'annotation');
-					$(annotation).css('left', x+"px");
-					$(annotation).css('top', y+"px");
-					
-					//Create remove button
-					var remove = $(document.createElement('a')).attr('class', 'button remove');
-					$(remove).attr('href', 'javascript:void(0)');
-					$(remove).html('x');
-					$(remove).click(function(){$(this).parent().parent().remove();});
-					
-					//Create toggle button
-					var toggle = $(document.createElement('a')).attr('class', 'button toggle');
-					$(toggle).attr('href', 'javascript:void(0)');
-					$(toggle).html('~');
-					$(toggle).click(function(){minMax($(this).parent().parent());});
-
-					
-					var buttons = $(document.createElement('div')).attr('class', 'buttons');
-					
-					//Append textbox and buttons to annotation, and annotation to textbox
-					$(toggle).appendTo(buttons);
-					$(remove).appendTo(buttons);
-					$(buttons).appendTo(annotation);
-					$(textBox).appendTo(annotation);
-					
-					$(annotation).appendTo('#source');
-					
-					
-					
-					//Set focus
-					$(textBox).focus();
-					
-					//Clear tool selection
-					cancel();
+										
+					addAnnotation(x, y);
 				}
 			}
 		);
@@ -124,5 +75,95 @@ $(document).ready(
 		var minMax = function(toToggle) {
 			$(toToggle).children('textarea').toggle();
 		}
+		
+		var sizeTextBox = function(box) {
+			/* Make sure element does not have scroll bar to prevent jumpy-ness */
+		   if (box.style.overflowY != 'hidden') { box.style.overflowY = 'hidden' }
+		   /* Now adjust the height */
+		   var scrollH = box.scrollHeight;
+		   if( scrollH > box.style.height.replace(/[^0-9]/g,'') ){
+			  box.style.height = scrollH+'px';
+		   }
+		}
+		
+		var addAnnotation = function(left, top, text) {
+			//Create textbox
+			var textBox = $(document.createElement('textarea'));
+			$(textBox).keyup(
+				//Grow textbox with content
+				function() {
+				   sizeTextBox(this);
+				}
+			);
+			$(textBox).html(text);
+			
+			//Create annotation
+			var annotation = $(document.createElement('div')).attr('class', 'annotation');
+			$(annotation).css('left', left+"px");
+			$(annotation).css('top', top+"px");
+			
+			//Create remove button
+			var remove = $(document.createElement('a')).attr('class', 'button remove');
+			$(remove).attr('href', 'javascript:void(0)');
+			$(remove).html('x');
+			$(remove).click(function(){$(this).parent().parent().remove();});
+			
+			//Create toggle button
+			var toggle = $(document.createElement('a')).attr('class', 'button toggle');
+			$(toggle).attr('href', 'javascript:void(0)');
+			$(toggle).html('~');
+			$(toggle).click(function(){minMax($(this).parent().parent());});
+
+					
+			var buttons = $(document.createElement('div')).attr('class', 'buttons');
+				
+			//Append textbox and buttons to annotation, and annotation to textbox
+			$(toggle).appendTo(buttons);
+			$(remove).appendTo(buttons);
+			$(buttons).appendTo(annotation);
+			$(textBox).appendTo(annotation);
+					
+			$(annotation).appendTo('#source');
+			
+			//Set focus
+			$(textBox).focus();
+			$(textBox).keyup();
+			cancel();
+		}
+		
+		//Add URL annotations
+		var lefts = $.getQueryString('lefts').split('|');
+		var tops = $.getQueryString('tops').split('|');
+		var strings = $.getQueryString('strings').split('|');
+		
+		for(var x = 0; x < strings.length; x++) {
+			addAnnotation(lefts[x], tops[x], strings[x]);
+		}
 	}
 );
+
+;(function ($) {
+    $.extend({      
+        getQueryString: function (name) {           
+            function parseParams() {
+                var params = {},
+                    e,
+                    a = /\+/g,  // Regex for replacing addition symbol with a space
+                    r = /([^&=]+)=?([^&]*)/g,
+                    d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
+                    q = window.location.search.substring(1);
+
+                while (e = r.exec(q))
+                    params[d(e[1])] = d(e[2]);
+
+                return params;
+            }
+
+            if (!this.queryStringParams)
+                this.queryStringParams = parseParams(); 
+
+            return this.queryStringParams[name];
+        }
+    });
+})(jQuery);
+
